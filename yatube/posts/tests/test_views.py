3f6c8baa89ba58e03b5e61e8c337fd286e13bf9b
Андрей Алексеев <0306andrey@gmail.com>
2022-08-13 21:12:30 +0300

    Домашняя работа 5 v2

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -223,12 +223,12 @@ class PostViewTests(TestCase):
 
     def test_authorized_user_can_follow_another_user(self):
         """Один авторизированный пользователь может подписаться на другого."""
-        count = Follow.objects.check()
+        count = Follow.objects.count()
         self.follower_client.get(reverse(
             'posts:profile_follow',
             kwargs={'username': PostViewTests.user.username}
         ))
-        self.assertEqual(Follow.objects.check(), count + 1)
+        self.assertEqual(Follow.objects.count(), count + 1)
         self.assertTrue(
             Follow.objects.filter(
                 user=self.follower,
@@ -238,12 +238,12 @@ class PostViewTests(TestCase):
 
     def test_authorized_user_cant_follow_himself(self):
         """Авторизированный пользователь не может подписаться сам на себя."""
-        count = Follow.objects.check()
+        count = Follow.objects.count()
         self.follower_client.get(reverse(
             'posts:profile_follow',
             kwargs={'username': self.follower.username}
         ))
-        self.assertEqual(Follow.objects.check(), count)
+        self.assertEqual(Follow.objects.count(), count)
         self.assertFalse(
             Follow.objects.filter(
                 user=self.follower,
@@ -253,16 +253,16 @@ class PostViewTests(TestCase):
 
     def test_authorized_user_can_unfollow(self):
         """Авторизированный пользователь может отписаться."""
-        count = Follow.objects.check()
         Follow.objects.create(
             user=self.follower,
             author=PostViewTests.user
         )
+        count = Follow.objects.count()
         self.follower_client.get(reverse(
             'posts:profile_unfollow',
             kwargs={'username': PostViewTests.user.username}
         ))
-        self.assertEqual(Follow.objects.check(), count - 1)
+        self.assertEqual(Follow.objects.count(), count - 1)
         self.assertFalse(
             Follow.objects.filter(
                 user=self.follower,
